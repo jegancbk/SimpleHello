b1a19bb10becbd825fa44914582ea3aa16f47bc1
Jegan Kabilan <jegancbk@gmail.com>
2017-04-04 02:11:38 -0500

    Initial commit

---
+++ app/src/main/java/com/practice/simplehello/helloActivity.java
@@ -0,0 +1,50 @@
+package com.practice.simplehello;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.practice.uipages.LoadUIPages;
+
+public class helloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+
+        WebView webView =  (WebView) findViewById(R.id.helloWebView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(this), "LoginInterface");
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/hello.html");
+
+
+    }
+
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+
+        @JavascriptInterface
+        public void showLoginUI() {
+            Toast.makeText(mContext, "Test", Toast.LENGTH_SHORT).show();
+            LoadUIPages loadUIPages = new LoadUIPages(mContext);
+            loadUIPages.loadUI();
+        }
+    }
+}
